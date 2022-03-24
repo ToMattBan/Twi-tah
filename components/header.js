a14ce68bf7fa0ca50407bf22ca7aa3d207f181ac
ToMattBan <souaquelecleyton@gmail.com>
2022-03-23 21:23:14 -0300

    Fix wrong URL on header

--- components/header.js
+++ components/header.js
@@ -6,10 +6,12 @@ export default function Header() {
 
   return (
     <header className="posterrHeader _pot _t0 _bgsecondary _pxs _df _jcsb _z1 _aic _elevation16">
-      <Link href={`/homepage/?userId=${userId}`} as={`/userpage/${userId}`}>
+      <Link href={`/homepage?posts=all`}>
         <a className="_tdn"> <div className="_fw7 _fz20 _white">Posterr</div> </a>
       </Link>
-      <Image className="_bdrs50p _cp" src="https://randomuser.me/api/portraits/women/35.jpg" width="50%" height="50%" />
+      <Link href={`/homepage/?userId=${userId}`} as={`/userpage/${userId}`}>
+        <Image className="_bdrs50p _cp" src="https://randomuser.me/api/portraits/women/35.jpg" width="50%" height="50%" />
+      </Link>
     </header>
   )
 }
